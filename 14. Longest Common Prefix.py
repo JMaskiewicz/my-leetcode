f1ca5363cf49ec40eb3db7d29de609dcbbc4724c
jmask <j.maskiewicz@student.uw.edu.pl>
2024-06-19 19:33:09 +0200

    14. Longest Common Prefix

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -5,14 +5,20 @@ https://leetcode.com/problems/longest-common-prefix/description/
 
 from typing import List
 
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        mini, maxi = min(strs), max(strs)  # something like sorting
-        # next we compare only these 2 as if those 2 have common rest also need to have
+        # Find the lexicographically smallest and largest strings
+        mini, maxi = min(strs), max(strs)
 
+        # Iterate through the characters of the smallest string
         for i in range(len(mini)):
+            # Compare characters at the same position in both strings
             if mini[i] != maxi[i]:
+                # Return the substring from the start to the first differing character
                 return mini[:i]
+
+        # If no differing character is found, return the entire smallest string
         return mini
 
 
