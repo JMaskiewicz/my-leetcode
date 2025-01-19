7482b90cfa7ac57d57c33cba79f0e4f8720b245f
JMaskiewicz <j.maskiewicz@student.uw.edu.pl>
2025-01-19 21:04:43 +0100

    383. Ransom Note

---
+++ 383. Ransom Note.py
@@ -0,0 +1,17 @@
+"""
+383. Ransom Note
+https://leetcode.com/problems/ransom-note/description/
+"""
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magazine_count = {}
+        for char in magazine:
+            magazine_count[char] = magazine_count.get(char, 0) + 1
+
+        for char in ransomNote:
+            if magazine_count.get(char, 0) == 0:
+                return False
+            magazine_count[char] -= 1
+
+        return True
