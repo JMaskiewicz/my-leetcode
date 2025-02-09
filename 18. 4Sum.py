23298e0bcbb14098bc0938008b8613b3377c8c29
JMaskiewicz <j.maskiewicz@student.uw.edu.pl>
2025-02-09 17:52:08 +0100

    18. 4Sum v2

--- 18. 4Sum.py
+++ 18. 4Sum.py
@@ -5,6 +5,8 @@ https://leetcode.com/problems/4sum/
 
 from typing import List
 
+from typing import List
+
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
@@ -12,5 +14,28 @@ class Solution:
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            self.three_sum(nums, i, target, result)
+            self.threeSum(nums, i, target, result)
         return result
+
+    def threeSum(self, nums: List[int], index: int, target: int, result: List[List[int]]):
+        for j in range(index + 1, len(nums)):
+            if j > index + 1 and nums[j] == nums[j - 1]:
+                continue
+            self.twoSum(nums, index, j, target, result)
+
+    def twoSum(self, nums: List[int], first: int, second: int, target: int, result: List[List[int]]):
+        left, right = second + 1, len(nums) - 1
+        while left < right:
+            total = nums[first] + nums[second] + nums[left] + nums[right]
+            if total == target:
+                result.append([nums[first], nums[second], nums[left], nums[right]])
+                left += 1
+                right -= 1
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
+            elif total < target:
+                left += 1
+            else:
+                right -= 1
