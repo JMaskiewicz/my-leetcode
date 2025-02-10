24d104f0f360e52a99fa023948e2e55dcfc756a2
JMaskiewicz <j.maskiewicz@student.uw.edu.pl>
2025-02-10 14:21:44 +0100

    31. Next Permutation

--- 31. Next Permutation.py
+++ 31. Next Permutation.py
@@ -6,19 +6,13 @@ https://leetcode.com/problems/next-permutation/
 from typing import List
 
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+    def nextPermutation(self, nums):
         i = len(nums) - 2
         while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
-
         if i >= 0:
             j = len(nums) - 1
             while j >= 0 and nums[j] <= nums[i]:
                 j -= 1
-
             nums[i], nums[j] = nums[j], nums[i]
-
-        self.reverse(nums, i + 1)
+        nums[i + 1:] = reversed(nums[i + 1:])
